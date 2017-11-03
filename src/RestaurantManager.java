acf7a5f5e3872940b8ed0904af0b7aafea6f35f3
pknn1 <pakanon.pk@gmail.com>
2017-11-04 00:16:23 +0700

    Rebuild code in Mr.James Objective // Still have TODO

--- src/RestaurantManager.java
+++ src/RestaurantManager.java
@@ -1,192 +1,126 @@
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
+/**
+ * RestaurantManager is a manager class for SKERestaurant
+ * Contain helper method such as collect menu from a file,
+ * record menuOrder log in a file, save data for menu
+ *
+ * @author Pakanon Pantisawat
+ */
 class RestaurantManager {
+    private static String[] menuItems;
+    private static double[] menuPrices;
+    private static String[] discountItems;
+    private static int itemsCount;
 
-    private static Scanner input = new Scanner(System.in);
+    private RestaurantManager() {
+        init();
+    }
 
-    private List<String> menuName = new ArrayList<>();
-    private List<String> order = new ArrayList<>();
-    private List<Double> menuPrice = new ArrayList<>();
-    private int[] menuQuantity;
+    /**
+     * Get InputStream and return if file is exist
+     *
+     * @param menuFile is a relative file path
+     * @return InputStream if file was found or none
+     */
+    private static InputStream getInputStream(String menuFile) {
+        ClassLoader cl = RestaurantManager.class.getClassLoader();
+        return cl.getResourceAsStream(menuFile);
+    }
 
-    public void recordOrder(int orderNumber, String[] order, double total) throws IOException {
-        String logFilePath = "data/orderLog.log";
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(logFilePath);
-        } catch (FileNotFoundException ex) {
-            System.out.println("Couldn't open output file " + logFilePath);
+    /**
+     * Count menu in menuFile and return it as integer
+     *
+     * @param fileName fileName of menuFile
+     * @return total number of menu in file
+     */
+    private static int getMenuItemsCount(String fileName) {
+        int lineCount = 0;
+        InputStream is = getInputStream(fileName);
+        if (is == null) {
+            System.err.printf("File %s was not found%n", fileName);
+            return 0;
+        }
+
+        Scanner fileScanner = new Scanner(is);
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            if (line.startsWith("#") || line.equals("")) continue;
+            lineCount++;
+        }
+        fileScanner.close();
+
+        return lineCount;
+    }
+
+    /**
+     * Read menu data and store it in array
+     *
+     * @param fileName
+     */
+    static void setMenu(String fileName) {
+        InputStream is = getInputStream(fileName);
+        if (is == null) {
+            System.err.printf("File %s was not found%n", fileName);
             return;
         }
-        PrintStream ps = new PrintStream(os);
-        ps.println("Order number: " + orderNumber);
-        for (String anOrder : order) {
-            System.out.println(anOrder);
+
+        itemsCount = getMenuItemsCount(fileName);
+        menuItems = new String[itemsCount];
+        menuPrices = new double[itemsCount];
+
+        Scanner fileScanner = new Scanner(is);
+        int menuIndex = 0;
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            if (line.startsWith("#") || line.equals("")) continue;
+            menuItems[menuIndex] = line.split(";")[0];
+            menuPrices[menuIndex] = Double.parseDouble(line.split(";")[1].replaceAll(" ", ""));
+            menuIndex++;
         }
-        ps.println("Total: " + total);
-        ps.close();
+        fileScanner.close();
     }
 
-    private void setMenu() {
-        String menuPath = "data/menuFile.txt";
-        setMenu(menuPath);
+    static String[] getMenuItems() {
+        return menuItems;
     }
 
-    private void setMenu(String fileName) {
-        ClassLoader cl = RestaurantManager.class.getClassLoader();
-        InputStream fi = cl.getResourceAsStream(fileName);
-        Scanner menuScanner = new Scanner(fi);
-        int count = 0;
-        while (menuScanner.hasNextLine()) {
-            String line = menuScanner.nextLine();
-            if (line.equals("") || line.startsWith("#")) {
-                continue;
-            }
-            count += 1;
-            menuName.add(line.split(";")[0]);
-            menuPrice.add(Double.valueOf(line.split(";")[1].replaceAll(" ", "")));
-        }
-        menuQuantity = new int[count];
-        menuScanner.close();
+    static double[] getMenuPrices() {
+        return menuPrices;
     }
 
-    void init() throws IOException {
-        setMenu();
-        String logFilePath = "data/orderLog.log";
+    /**
+     * Get items count and allocate memory for menuItems and menuPrices,
+     * then call setMenu() for read menu from menuFile
+     */
+    static void init() {
+        String menuFilePath = "data/menuFile.txt";
+        setMenu(menuFilePath);
+    }
+
+    /**
+     * Record menuOrder log to text file for further usage
+     *
+     * @param orderNumber menuOrder number of menuOrder
+     * @param order       array of menuOrder listing
+     * @param total       total price for menuOrder
+     */
+    public void recordOrder(int orderNumber, int[] order, double total) {
+        String orderFilePath = "data/orderLog.txt";
         try {
-            OutputStream os = new FileOutputStream(logFilePath);
-        } catch (IOException e) {
-            System.out.println("Log File could not be found");
-        }
-    }
+            FileWriter fw = new FileWriter(orderFilePath, true);
+            PrintWriter pw = new PrintWriter(fw);
 
-    /**
-     * Check if there is "menuFile.txt" existing in the directory
-     * and put all menu name and price in the array list.
-     * If it doesn't exist, the method will ask user if they want to create new menu
-     */
-    public String[] getMenuItems() {
-        String[] name = new String[menuName.size()];
-        menuName.toArray(name);
-        return name;
-    }
-
-    public double[] getPrices() {
-        double[] price = new double[menuPrice.size()];
-        for (int i = 0; i < menuPrice.size(); i++) {
-            price[i] = menuPrice.get(i);
-        }
-        return price;
-    }
-
-    /**
-     * Method for print entire menu from the list
-     */
-    private void printMenu() {
-        // print menu
-        for (int i = 0; i < menuName.size(); i++) {
-            System.out.printf("%2d. %-20s -- %.2f Baht%n", i + 1, menuName.get(i), menuPrice.get(i));
-        }
-    }
-
-    /**
-     * Print the menu of food and drinks,
-     * order by put order code or menu name following by / and quantity
-     * check for valid input then add to the menu
-     */
-    void orderFood() {
-        printMenu();
-        System.out.println("How to order: (menu name / code)(/)(quantity)");
-        System.out.print("What do you want to order: ");
-        String[] order = input.nextLine().split("/");
-
-        // check if quantity is integer
-        if (order[1].charAt(0) > 57) System.out.println("Quantity is invalid");
-
-        // check if user input menu name or order code
-        if (order[0].charAt(0) > 57) {
-            // check if menu name is exist
-            if (menuName.indexOf(order[0]) != -1) {
-                menuQuantity[menuName.indexOf(order[0])] += Integer.valueOf(order[1]);
-                System.out.printf("%s added for %s%n", order[0], order[1]);
-            } else {
-                System.out.println("Menu name is invalid");
+            pw.printf("Order Number: %05d%n", orderNumber);
+            for (int anOrder : order) {
+                pw.printf("%010d%n", anOrder);
             }
-        } else {
-            int orderCode = Integer.valueOf(order[0]) - 1;
-
-            // check if order code is valid
-            if (orderCode + 1 > menuQuantity.length || orderCode < 0) System.out.println("Order code is invalid");
-            else {
-                menuQuantity[orderCode] += Integer.valueOf(order[1]);
-                System.out.printf("%s added for %s%n", menuName.get(orderCode), order[1]);
-            }
+            pw.printf("Total : %.2f Baht%n", total);
+            pw.println();
+            pw.close();
+        } catch (IOException ioe) {
+            System.err.printf("Cannot access file %s", orderFilePath);
         }
     }
-
-
-    /**
-     * Simply print current bill and check if user want to check out
-     *
-     * @param isCheckOut is true if user want to check out
-     */
-    void checkOrder(boolean isCheckOut) throws IOException {
-        double totalPrice = 0.00;
-
-        System.out.println();
-        System.out.println("Latest bill is: ");
-        System.out.println("----------------------------------------------");
-        System.out.printf("::%3s %-20s :: %3s :: %6s ::%n", "No.", "Menu", "Qt", "Price");
-        System.out.println("----------------------------------------------");
-
-        for (int i = 0, j = 1; i < menuName.size(); i++) {
-            if (menuQuantity[i] != 0) {
-                double eachNetPrice = menuPrice.get(i) * menuQuantity[i];
-                totalPrice += eachNetPrice;
-                System.out.printf("::%2d. %-20s :: %3d :: %6.2f ::%n", j, menuName.get(i), menuQuantity[i], eachNetPrice);
-                j++;
-                order.add(menuName.get(i));
-            }
-        }
-
-        if (isCheckOut) {
-            double discount = totalPrice * getDiscount() / 100;
-            System.out.printf("::%-31s :: -%5.2f ::%n", "Discount", discount);
-            totalPrice -= discount;
-            String[] theOrder = new String[order.size()];
-            order.toArray(theOrder);
-            Random rand = new Random();
-            int orderNum = rand.nextInt(10000) + 1;
-            recordOrder(orderNum, theOrder, totalPrice);
-        }
-
-        System.out.println("----------------------------------------------");
-        System.out.printf("::%31s :: %6.2f ::%n", "Total", totalPrice);
-        System.out.println("----------------------------------------------");
-        System.out.println();
-    }
-
-    /**
-     * Check if user want to use coupon
-     * and check if coupon is valid
-     *
-     * @return random value from (5, 10, 15) for discount percent
-     */
-    private double getDiscount() {
-        Random rand = new Random();
-        System.out.print("Do you have any Coupon (y/n) ** One coupon per bill** : ");
-        if (input.next().toLowerCase().charAt(0) == 'y') {
-            System.out.println("Coupon code must starts with SKE and following by 8 digit : Example (SKE01930024)");
-            while (true) {
-                System.out.print("Input coupon code: ");
-                String couponCode = input.nextLine();
-                if (couponCode.startsWith("SKE") && couponCode.length() == 11) {
-                    return (rand.nextInt(3) + 1) * 5;
-                } else System.out.println("Invalid input");
-            }
-        }
-        return 0;
-    }
-
 }
--- src/SKERestaurant.java
+++ src/SKERestaurant.java
@@ -1,53 +1,118 @@
-import java.io.*;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Scanner;
 
-public class SKERestaurant {
+/**
+ * SKERestaurant is an app for restaurant
+ * App can take an menuOrder, check menuOrder, calculate total price.
+ *
+ * @author Pakanon Pantisawat
+ */
+
+class SKERestaurant {
+
+    final static Scanner sc = new Scanner(System.in);
+    static String[] menuItems;
+    static double[] menuPrices;
+    static int[] menuOrder;
+
+    public static void main(String[] args) {
+        RestaurantManager.init();
+        if (args.length > 1 && args[0].equals("-m")) {
+            RestaurantManager.setMenu(args[1]);
+        }
+        menuItems = RestaurantManager.getMenuItems();
+        menuPrices = RestaurantManager.getMenuPrices();
+        menuOrder = new int[menuItems.length];
+        openSKERestaurant();
+    }
 
     /**
-     * Console dialog interprets the user's input
-     * and call RestaurantManager method from the object.
+     * Wait for user's input and call a responsive method
+     * also call for initialization method
      */
-    private static void openSKERestaurant() throws IOException {
-        RestaurantManager manager = new RestaurantManager();
-        Scanner input = new Scanner(System.in);
-        boolean isOpen = true;
+    private static void openSKERestaurant() {
 
-        System.out.println("Welcome to SKE Restaurant");
-        manager.init();
-        while (isOpen) {
-            System.out.println("What do you want to do?");
-            System.out.print("(o)rder, (c)heck order, check ou(t): ");
-            switch (input.next().charAt(0)) {
-                case 'o':
-                    manager.orderFood();
+        System.out.println(" _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ ");
+        System.out.println("|   __|  |  |   __| __  |   __|   __|_   _|  _  |  |  | __  |  _  |   | |_   _|");
+        System.out.println("|__   |    -|   __|    -|   __|__   | | | |     |  |  |    -|     | | | | | |");
+        System.out.println("|_____|__|__|_____|__|__|_____|_____| |_| |__|__|_____|__|__|__|__|_|___| |_|");
+        while (true) {
+            printMenu();
+            String input = sc.nextLine();
+            switch (input) {
+                case "p":
+                    placeOrder();
                     break;
-                case 'c':
-                    manager.checkOrder(false);
+                case "c":
+                    checkOrder();
                     break;
-                case 't':
-                    manager.checkOrder(true);
-                    isOpen = false;
+                case "d":
+                    // TODO: 11/3/17 Check Promotion
                     break;
+                case "o":
+                    // TODO: 11/3/17 Check out
+                    System.out.println("Good bye!!!");
+                    break;
+                case "?":
+                    printMenu();
+                    break;
+                case "s":
+                    System.exit(0);
                 default:
+                    System.out.println("Invalid menu");
                     break;
             }
         }
+    }
 
-        // Check if local time is night or day
-        int time = LocalDateTime.now().getHour();
-        if (time >= 18 || time <= 7) System.out.println("Have a good night!!!");
-        else System.out.println("Have a good day!!!");
+    private static void printMenu() {
+        System.out.println();
+        System.out.println("\"p\"  Place menuOrder");
+        System.out.println("\"c\"  Check menuOrder");
+        System.out.println("\"d\"  Check promotion");
+        System.out.println("\"o\"  Check out");
+        System.out.println("\"?\" See this menu again");
+        System.out.print("cmd> ");
     }
 
     /**
-     * Run the application
+     * Print all the menu items we have
+     * ask user for input menu number and amount they want
+     * then add to menuOrder array
      */
-    public static void main(String[] args) {
-        try {
-            openSKERestaurant();
-        } catch (IOException e) {
-            System.exit(0);
+    private static void placeOrder() {
+        System.out.println();
+        System.out.println("Menu : ");
+        for (int i = 0; i < menuItems.length; i++) {
+            System.out.printf("%2d. %-30s Price: %8.2f Baht%n", i + 1, menuItems[i], menuPrices[i]);
         }
+        System.out.println();
+
+        System.out.print("What would you like to menuOrder (by menu number) : ");
+        int orderNo = sc.nextInt();
+        System.out.print("How many of them: ");
+        int amount = sc.nextInt();
+        menuOrder[orderNo - 1] += amount;
+
+        System.out.printf("You've ordered %s for %d piece(s)%n%n", menuItems[orderNo - 1], menuOrder[orderNo - 1]);
     }
+
+    /**
+     * Print all order user have places and total price.
+     * Formatted text
+     */
+    private static void checkOrder() {
+        double total = 0;
+        System.out.println();
+        System.out.println("Your total order : ");
+        for (int i = 0, j = 1; i < menuItems.length; i++) {
+            if (menuOrder[i] == 0) continue;
+            double price = menuOrder[i] * menuPrices[i];
+            total += price;
+            System.out.printf("%2d. %-30s  Amount: %3d  Price: %8.2f Baht%n", j, menuItems[i], menuOrder[i], price);
+            j++;
+        }
+        System.out.printf("%54s: %8.2f Baht%n", "Total", total);
+    }
+
+
 }
